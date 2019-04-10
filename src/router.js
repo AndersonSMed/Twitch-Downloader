540bab0628e62c7e420f21a456eacf82667adce0
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-04-09 21:17:41 -0300

    Trying to upload site

--- src/router.js
+++ src/router.js
@@ -9,7 +9,8 @@ export default new Router({
     base: '/',
     routes: [
         {
-            path: '',
+            path: '/',
+            name: 'home',
             component: Home
         }
     ]
