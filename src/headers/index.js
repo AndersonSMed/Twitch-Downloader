eac17dd1499ec093c5afebc9c0e07b569b996d69
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-04-23 00:02:46 -0300

    New Version of the site

--- src/headers/index.js
+++
@@ -1,5 +0,0 @@
-// If you are using this project, get your own Client-ID and use it here
-export default {
-    'Client-ID': '3n1fwxo1kfd6xiyiymkltmf2bjut1f',
-    'Accept': 'application/vnd.twitchtv.v5+json'
-}
